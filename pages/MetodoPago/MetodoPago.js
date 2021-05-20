dc6d021e0dbcb9315e58b972abb5c9de6348cafa
gonzalo <goyarzunpanes@gmail.com>
2021-05-20 00:05:28 -0400

    se agregaron los componentes imagen, box pago, la vista metodo pago y se creo la vista tipo compra

---
+++ pages/MetodoPago/MetodoPago.js
@@ -0,0 +1,21 @@
+import BoxPago from "../../components/BoxPago";
+import Header from "../../components/Header";
+
+const PantallaMetodoPago = () => {
+  return (
+    <>
+      <Header />
+
+      <section>
+        <h2 className="sub-titulo-retiro-local">Seleccionar Método de Pago</h2>
+      </section>
+
+      <section className="d-flex">
+        <BoxPago titulo="Transferencia Electrónica" imagenCentral="https://res.cloudinary.com/repro/image/upload/v1621190800/logo_efectivo_ms6zjw.jpg" />
+        <BoxPago titulo="Pagar por Stripe" imagenCentral="https://res.cloudinary.com/repro/image/upload/v1621217353/stripe-payment-logo_tpz97u.png" />
+      </section>
+    </>
+  );
+};
+
+export default PantallaMetodoPago;
