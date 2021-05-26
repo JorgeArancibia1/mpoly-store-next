b7b510a843f5d1511ef913872592f9ea8377a4e4
gonzalo <goyarzunpanes@gmail.com>
2021-05-26 18:06:52 -0400

    x

--- components/Modal/DinamicModal/DinamicModal.js
+++ components/Modal/DinamicModal/DinamicModal.js
@@ -1,12 +1,27 @@
-import React from 'react'
+import { Button, Form } from "semantic-ui-react";
+import PublishIcon from '@material-ui/icons/Publish';
 
-
-const DinamicModal = ({isEditable=false}) => {
+const DinamicModal = ({ isEditable = false }) => {
   return (
     <div>
-      AQUI VA EL CODIGO DEL MODAL
+      <Form className="container-form">
+        <Form.Input type="text" name="nombre" placeholder="Nombre" />
+        <Form.Input type="text" name="marca" placeholder="Marca" />
+        <Form.Input type="text" name="color" placeholder="Color" />
+        <Form.Input type="text" name="talla" placeholder="Talla" />
+        <Form.Input type="text" name="tipo" placeholder="Tipo" />
+        <Form.Input type="text" name="categoria" placeholder="Categoria" />
+        <Form.Input type="text" name="precio" placeholder="Precio" />
+      </Form>
+      <div className="contenedor-boton">
+        <Button>Aceptar</Button>
+      </div>
+      <div>
+      <PublishIcon className="icono-contenedor"></PublishIcon>
+      <p>Agregar Imagen</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default DinamicModal
+export default DinamicModal;
